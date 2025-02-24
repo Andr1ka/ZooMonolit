75274a062d7e5eac2a79ed5f92cdefd8fe7511bc
Andrei <kuznechikovandrei@mail.ru>
2025-02-24 16:29:20 +0300

    Add documentation for ZooController

--- Zoo.API/Controllers/AnimalsController.cs
+++ Zoo.API/Controllers/AnimalsController.cs
@@ -4,18 +4,33 @@ using Zoo.Core.Interfaces;
 
 namespace Zoo.API.Controllers
 {
+    /// <summary>
+    /// Контроллер для управления животными в зоопарке
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class AnimalsController : ControllerBase
     {
         private readonly IAnimalService _animalService;
 
+        /// <summary>
+        /// Конструктор контроллера
+        /// </summary>
+        /// <param name="animalService">Сервис для работы с животными</param>
         AnimalsController(IAnimalService animalService)
         {
             _animalService = animalService;
         }
 
+        /// <summary>
+        /// Получение всех животных
+        /// </summary>
+        /// <returns>Список животных</returns>
+        /// <response code="200">Успешное выполнение. Возврат информации о всех существующих животных</response>
+        /// <response code="204">Ни одного животного еще не добавлено</response>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public async Task<IActionResult> GetAllAnimals()
         {
              var response = await _animalService.GetAllAnimals();
@@ -30,7 +45,16 @@ namespace Zoo.API.Controllers
 
         }
 
+        /// <summary>
+        /// Получение информации о животном по его id
+        /// </summary>
+        /// <param name="id">Идентификатор животного.</param>
+        /// <returns>Информация о животном</returns>
+        /// <response code="200">Успешное выполнение. Возврат информации о животном</response>
+        /// <response code="404">Животное с заданным id не найдено</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetById(int id)
         {
             var response = await _animalService.GetById(id);
@@ -44,7 +68,16 @@ namespace Zoo.API.Controllers
             }
         }
 
+        /// <summary>
+        /// Добавление нового животного
+        /// </summary>
+        /// <param name="animal">Данные нового животного.</param>
+        /// <returns>Информация о добавленном животном</returns>
+        /// <response code="201">Успешное выполнение. Возврат информации о добавленном животном</response>
+        /// <response code="400">Добавление завершилось с ошибкой</response>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> AddNewAnimal([FromBody] Animal animal)
         {
             var response = await _animalService.AddNewAnimal(animal);
@@ -57,7 +90,19 @@ namespace Zoo.API.Controllers
             }
         }
 
+        /// <summary>
+        /// Кормление животного с указанным id
+        /// </summary>
+        /// <param name = "id" > Идентификатор животного.</param>
+        /// <param name="food">Количество пищи для кормления.</param>
+        /// <returns>Результат операции кормления.</returns>
+        /// <response code="200">Успешное выполнение</response>
+        /// <response code="404">Животное с заданным id не найдено</response>
+        /// <response code="400">Кормление завершилось с ошибкой</response>
         [HttpPut("{id}/feed")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> FeedAnimal(int id,byte food)
         {
             var IsExisting = await _animalService.GetById(id);
@@ -77,8 +122,16 @@ namespace Zoo.API.Controllers
             }
         }
 
-
+        /// <summary>
+        /// Удаление животного c указанным id
+        /// </summary>
+        /// <param name="id">Идентификатор животного.</param>
+        /// <returns>Результат операции удаления.</returns>
+        /// <response code="204">Успешное выполнение</response>
+        /// <response code="404">Животное с заданным id не найдено</response>
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> DeleteAnimal(int id) 
         {
             var response = await _animalService.DeleteAnimal(id);
